80f9e4a7384914923783be2380e4100eb754ba7e
LoganArnett <Loganarn5@gmail.com>
2014-12-25 16:19:14 -0500

    ng-src fix

--- js/app.js
+++ js/app.js
@@ -2,14 +2,13 @@
 
 angular.module('slider', ['ngAnimate', 'ngTouch'])
 .controller('MainCtrl', function () {
-  console.log("Hello");
   this.slides = [
   { pic: 'images/activ8.png', description: 'Image 00'},
   { pic: 'images/vertical.png', description: 'Image 01'},
   { pic: 'images/surf.png', description: 'Image 02'},
   { pic: 'images/iron.png', description: 'Image 03'}
   ];
-  console.log(this.slides);
+  
   this.direction = 'left';
   this.currentIndex = 0;
 
