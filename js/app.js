83255b0f626de3178835ad6459295cf6a0ccbb49
LoganArnett <Loganarn5@gmail.com>
2014-12-25 18:35:32 -0500

    added mvctodo to projects slider and colored dots

--- js/app.js
+++ js/app.js
@@ -7,10 +7,11 @@ angular.module('slider', ['ngAnimate', 'ngTouch'])
   .controller('MainCtrl', function () {
     var self = this;
     self.slides = [
-    {image: 'images/activ8.png', description: 'Image 00'},
-    {image: 'images/vertical.png', description: 'Image 01'},
-    {image: 'images/surf.png', description: 'Image 02'},
-    {image: 'images/iron.png', description: 'Image 03'}
+    {image: 'images/activ8.png', description: 'Image 00', url: "http://activ8.fitness/crosstrackapp/"},
+    {image: 'images/vertical.png', description: 'Image 01', url: "http://loganarnett.com/TIY-Assignments/HTML-CSS/Timeline/Vertical-Timeline.html"},
+    {image: 'images/surf.png', description: 'Image 02', url: "http://loganarnett.com/TIY-Assignments/Surf-and-Paddle/index.html"},
+    {image: 'images/iron.png', description: 'Image 03', url: "http://loganarnett.com/TIY-Assignments/HTML-CSS/Random/19--Build-a-Blog.html"}
+    {image: 'images/mvctodo.png', description: 'Image 04', url: "http://loganarnett.com/TIY-Assignments/MVCtoDo/jQuery-Version/index.html"}
     ];
     this.direction = 'left';
     this.currentIndex = 0;
