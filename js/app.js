298cbe6f34981487c3b19fd7c59b38c326761926
LoganArnett <Loganarn5@gmail.com>
2015-01-16 10:13:15 -0500

    added vertical timeline link back in

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ angular.module('slider', ['ngAnimate', 'ngTouch'])
   var self = this;
   self.slides = [
   {image: 'images/activ8.png', description: 'Image 00', url: "http://activ8.fitness/crosstrackapp/"},
-  {image: 'images/vertical.png', description: 'Image 01', url: "http://htmlpreview.github.io/?https://github.com/LoganArnett/TIY-Assignments/blob/gh-pages/HTML-CSS/Timeline/Vertical-Timeline.html"},
+  {image: 'images/vertical.png', description: 'Image 01', url: "http://loganarnett.com/TIY-Assignments/HTML-CSS/Timeline/Vertical-Timeline.html"},
   {image: 'images/surf.png', description: 'Image 02', url: "http://loganarnett.com/TIY-Assignments/Surf-and-Paddle/index.html"},
   {image: 'images/iron.png', description: 'Image 03', url: "http://loganarnett.com/TIY-Assignments/HTML-CSS/Random/19--Build-a-Blog.html"},
   {image: 'images/mvctodo.png', description: 'Image 04', url: "http://loganarnett.com/TIY-Assignments/MVCtoDo/jQuery-Version/index.html"}
