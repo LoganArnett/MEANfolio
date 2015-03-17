4d4c933b99e61a53b4bdf01f8abdced236dec36e
LoganArnett <Loganarn5@gmail.com>
2015-03-16 23:13:28 -0400

    updating slider projects and adding captions

--- js/app.js
+++ js/app.js
@@ -7,11 +7,11 @@ angular.module('slider', ['ngAnimate', 'ngTouch'])
 .controller('MainCtrl', function () {
   var self = this;
   self.slides = [
-  {image: 'images/activ8.png', description: 'Image 00', url: "http://activ8.fitness/crosstrackapp/"},
-  {image: 'images/vertical.png', description: 'Image 01', url: "http://loganarnett.com/TIY-Assignments/HTML-CSS/Timeline/Vertical-Timeline.html"},
-  {image: 'images/surf.png', description: 'Image 02', url: "http://loganarnett.com/TIY-Assignments/Surf-and-Paddle/index.html"},
-  {image: 'images/iron.png', description: 'Image 03', url: "http://loganarnett.com/TIY-Assignments/HTML-CSS/Random/19--Build-a-Blog.html"},
-  {image: 'images/mvctodo.png', description: 'Image 04', url: "http://loganarnett.com/TIY-Assignments/MVCtoDo/jQuery-Version/index.html"}
+  {image: 'images/activ8.png', description: 'Image 00', url: "http://activ8.fitness/crosstrackapp/", caption: 'Fitness Tracking Web App built with AngularJS and Firebase as a final project at The Iron Yard'},
+  {image: 'images/wildenglish.png', description: 'Image 01', url: "http://http://loganarnett.com/wildenglish/app/#/main", caption: 'Wild English Band homepage with Soundcloud and Google Calendar integrations'},
+  {image: 'images/surf.png', description: 'Image 02', url: "http://loganarnett.com/TIY-Assignments/Surf-and-Paddle/index.html", caption: 'Mock up of a Surf company blog and info site'},
+  {image: 'images/ngstore.png', description: 'Image 03', url: "http://loganarnett.com/NgStoreFront/#/", caption: 'Single page store front web app built with Angular and filled with products from the Etsy API'},
+  {image: 'images/mvctodo.png', description: 'Image 04', url: "http://loganarnett.com/TIY-Assignments/MVCtoDo/jQuery-Version/index.html", caption: 'jQuery implementation of the TodoMVC project'}
   ];
   console.log(this.slides)
   this.direction = 'left';
