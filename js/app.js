51a6a309dde4c25cff4e7963a771894dc28bde08
LoganArnett <Loganarn5@gmail.com>
2015-04-05 13:06:51 -0400

    fixed dead link

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ angular.module('slider', ['ngAnimate', 'ngTouch'])
   var self = this;
   self.slides = [
   {image: 'images/activ8.png', description: 'Image00', url: "http://activ8.fitness/crosstrackapp/", caption: 'Fitness Tracking Web App built with AngularJS and Firebase'},
-  {image: 'images/wildenglish.png', description: 'Image01', url: "http://http://loganarnett.com/wildenglish/app/#/main", caption: 'Wild English site with Soundcloud and Google Calendar integrations'},
+  {image: 'images/wildenglish.png', description: 'Image01', url: "http://loganarnett.com/wildenglish/app/#/main", caption: 'Wild English site with Soundcloud and Google Calendar integrations'},
   {image: 'images/surf.png', description: 'Image02', url: "http://loganarnett.com/TIY-Assignments/Surf-and-Paddle/index.html", caption: 'Mock up of a Surf company blog and info site'},
   {image: 'images/ngstore.png', description: 'Image03', url: "http://loganarnett.com/NgStoreFront/#/", caption: 'Single page store front web app built with Angular and filled with products from the Etsy API'},
   {image: 'images/mvctodo.png', description: 'Image04', url: "http://loganarnett.com/TIY-Assignments/MVCtoDo/jQuery-Version/index.html", caption: 'jQuery implementation of the TodoMVC project'}
