deb9e07dfcdd24f2201270e2671b0c9cc3f670b8
LoganArnett <Loganarn5@gmail.com>
2015-05-13 13:44:23 -0400

    autojekyll

--- js/app.js
+++ js/app.js
@@ -9,9 +9,10 @@ angular.module('slider', ['ngAnimate', 'ngTouch'])
   self.slides = [
   {image: 'images/activ8.png', description: 'Image00', url: "http://activ8.fitness/crosstrackapp/", caption: 'Fitness Tracking Web App built with AngularJS and Firebase'},
   {image: 'images/wildenglish.png', description: 'Image01', url: "http://loganarnett.com/wildenglish/app/#/main", caption: 'Wild English site with Soundcloud and Google Calendar integrations'},
-  {image: 'images/surf.png', description: 'Image02', url: "http://loganarnett.com/TIY-Assignments/Surf-and-Paddle/index.html", caption: 'Mock up of a Surf company blog and info site'},
-  {image: 'images/ngstore.png', description: 'Image03', url: "http://loganarnett.com/NgStoreFront/#/", caption: 'Single page store front web app built with Angular and filled with products from the Etsy API'},
-  {image: 'images/mvctodo.png', description: 'Image04', url: "http://loganarnett.com/TIY-Assignments/MVCtoDo/jQuery-Version/index.html", caption: 'jQuery implementation of the TodoMVC project'}
+  {image: 'images/autojekyll.png', description: 'Image02', url: "http://loganarnett.com/AutoJekyll/#/new", caption: 'AutoJekyll tool built for Jekyll Blog workflow'},
+  {image: 'images/surf.png', description: 'Image03', url: "http://loganarnett.com/TIY-Assignments/Surf-and-Paddle/index.html", caption: 'Mock up of a Surf company blog and info site'},
+  {image: 'images/ngstore.png', description: 'Image04', url: "http://loganarnett.com/NgStoreFront/#/", caption: 'Single page store front web app built with Angular and filled with products from the Etsy API'},
+  {image: 'images/mvctodo.png', description: 'Image05', url: "http://loganarnett.com/TIY-Assignments/MVCtoDo/jQuery-Version/index.html", caption: 'jQuery implementation of the TodoMVC project'}
   ];
   console.log(this.slides)
   this.direction = 'left';
