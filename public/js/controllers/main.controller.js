132fb7deb1b1bf0a52189429a3730b3d5071c2ee
LoganArnett <Loganarn5@gmail.com>
2015-09-04 17:22:13 -0400

    fixed slide out menu fade

--- public/js/controllers/main.controller.js
+++ public/js/controllers/main.controller.js
@@ -30,17 +30,19 @@ angular.module('scotchMean')
 
   this.hamburger = function(e){
     var meat = angular.element(document.querySelector( '.menu-icon' ));
-    var menu = angular.element(document.querySelector( '.navigation' ));
+    var menu = angular.element(document.querySelector( '#menuFade' ));
     console.log(meat)
     meat.toggleClass('active');
-    menu.toggleClass('slideIn');
+    menu.toggleClass('is-visible');
     setTimeout(function() {
         meat.toggleClass('done');
     }, 400);
   }
 
   this.sidePanel = function(e){
-    angular.element(document.querySelector('.sliding-panel-content,.sliding-panel-fade-screen')).toggleClass('is-visible');
+    angular.element(document.querySelector('.sliding-panel-content')).toggleClass('is-visible');
+    angular.element(document.querySelector('.sliding-panel-fade-screen')).toggleClass('is-visible');
+    // angular.element(document.querySelector('.sliding-panel-fade-screen')).toggleClass('is-visible');
     angular.element(document.querySelector('.menu-icon')).toggleClass('active');
     setTimeout(function(){angular.element(document.querySelector('.menu-icon')).toggleClass('done')}, 300)
     e.preventDefault();
